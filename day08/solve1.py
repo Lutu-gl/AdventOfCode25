d3f47b093de9b9dd8ba011eb1fc474347bf6da1d
Luca Turin <74654393+Lutu-gl@users.noreply.github.com>
2025-12-09 17:13:13 -0600

    day 8 🦌

---
+++ day08/solve1.py
@@ -0,0 +1,52 @@
+import heapq
+import math
+from itertools import combinations
+
+with open("input.txt", "r") as file:
+    points = [tuple(map(int, line.split(","))) for line in file.read().splitlines()]
+
+heap = []
+for point1, point2 in combinations(points, 2):
+        val = math.sqrt(math.pow(point1[0] - point2[0], 2) + math.pow(point1[1] - point2[1], 2) + math.pow(point1[2] - point2[2], 2))
+        heapq.heappush(heap, (val, point1, point2))
+
+sets = []
+
+for i in range(1000):
+    smallest = heapq.heappop(heap)
+    found = False
+
+    for set1 in sets:
+        if smallest[1] in set1:
+            found = True
+            set1.add(smallest[2])
+            for set2 in sets:
+                if set1 == set2:
+                    continue
+                if smallest[2] in set2:
+                    set1.update(set2)
+                    sets.remove(set2)
+        if smallest[2] in set1:
+            found = True
+            set1.add(smallest[1])
+            for set2 in sets:
+                if set1 == set2:
+                    continue
+                if smallest[1] in set2:
+                    set1.update(set2)
+                    sets.remove(set2)
+    if not found:
+        s = set()
+        s.add(smallest[1])
+        s.add(smallest[2])
+        sets.append(s)
+
+sets = sorted(sets, key=lambda x: len(x), reverse=True)
+
+val = 1
+for i in range(3):
+    if (i >= len(sets)):
+        break
+    val *= len(sets[i])
+
+print(val)
